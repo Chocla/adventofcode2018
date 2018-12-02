b581a82a57c7052a6bbdc23d763a26044f90bbad
Chocla <19178866+Chocla@users.noreply.github.com>
2018-12-01 18:10:31 -0800

    Clean Up Day 1 A bit

--- day01/main.go
+++ day01/main.go
@@ -1,51 +1,44 @@
 package main
 
 import (
-	"strconv"
+	"bufio"
 	"fmt"
 	"os"
-	"bufio"
+	"strconv"
 )
 
 var path = "input.txt"
 
 func main() {
-	fmt.Println("Part 1:",partOne(path),"\nPart 2:", partTwo(path))
+	fmt.Println("Part 1:", partOne(path), "\nPart 2:", partTwo(path))
 }
 
 //add numbers of file
 func partOne(path string) (freq int) {
-	file,_ := os.Open(path)
+	file, _ := os.Open(path)
 	scan := bufio.NewScanner(file)
 	for scan.Scan() {
-		freq += stringToSignedInt(scan.Text())
+		n, _ := strconv.Atoi(scan.Text())
+		freq += n
 	}
 	return
 }
-func partTwo(path string) (int) {
+func partTwo(path string) int {
 	freq := 0
 	//indices are frequencies we've seen
-	freqMap := make(map[int]bool) 
+	freqMap := make(map[int]bool)
+	freqMap[freq] = true
 	for {
-		file,_ := os.Open("input.txt")
+		file, _ := os.Open("input.txt")
 		scan := bufio.NewScanner(file)
 		for scan.Scan() {
-			freq += stringToSignedInt(scan.Text())
+			n, _ := strconv.Atoi(scan.Text())
+			freq += n
 			//if it's been seen, this is the second occurence
-			if freqMap[freq]{ 
+			if freqMap[freq] {
 				return freq
 			}
 			freqMap[freq] = true
 		}
 	}
-}
-
-//Converts Strings of the form +Num or -Num into their integer equivalent
-func stringToSignedInt(lineString string) (n int){
-	sign := lineString[0:1]
-	n,_ = strconv.Atoi(lineString[1:])
-	if sign == "-" {
-		n *= -1
-	}
-	return
 }
