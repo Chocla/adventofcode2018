656639301278802edbf4bf387850f2b89ff0d757
Chocla <19178866+Chocla@users.noreply.github.com>
2018-12-05 13:54:04 -0800

    Day 4 Part 1

--- day04/day4.py
+++ day04/day4.py
@@ -5,31 +5,28 @@ f = open('input.txt','r')
 entries = f.read().splitlines()
 entries.sort()
 
-guards = defaultdict(float)
+guards = defaultdict(int)
 for e in entries:
     if e[19] == "G":
-        #print e
         guard = e.split("#")[1].split(" ")[0]
         i = 0
         continue
     if i % 2 == 0:
         sleepTime =  datetime.datetime.strptime(e[1:17], "%Y-%m-%d %H:%M")
-        #print sleepTime.hour
     else:
         wakeTime = datetime.datetime.strptime(e[1:17], "%Y-%m-%d %H:%M")
-        guards[guard] +=  (wakeTime - sleepTime).total_seconds() / 60
-        
-        #print datetime.timedelta(sleepTime, wakeTime)
+        guards[guard] += int((wakeTime - sleepTime).total_seconds() / 60)
     i += 1
-#print guards
+
 maxTime = 0.0
+print len(guards)
 for g in guards:
-    #print guards[g] > 0.0
     if guards[g] > maxTime:
         maxTime = guards[g]
         maxGuard = g
-#Check only the correct guard's times for minutes
+
 minutes = defaultdict(int)
+
 for e in entries:
     if e[19] == "G":
         if e.split("#")[1].split(" ")[0] == maxGuard:
@@ -38,26 +35,22 @@ for e in entries:
             correctGuard = False
         i = 0
         continue
+
     if correctGuard and i % 2 == 0:
         start = datetime.datetime.strptime(e[1:17], "%Y-%m-%d %H:%M")
-    if correctGuard and i % 2 != 0:
+        
+    if correctGuard and i % 2 ==1:
         end = datetime.datetime.strptime(e[1:17], "%Y-%m-%d %H:%M")
         delta = int((end - start).total_seconds() / 60)
         startMinute = start.minute
-        endMinute = end.minute
-        #print start, end
-        for i in range(delta-1):
-            minutes[(startMinute+i)%60] += 1
-        #for each minute between start and end
-            # increment that minute's counter 
-        #print startMinute, range(delta+1)
+        for j in range(delta):
+            minutes[(startMinute+j)] += 1
     i += 1
-maxCount = 0
 
+maxCount = 0
 for m in minutes:
     if minutes[m] > maxCount:
         maxMinute = m
         maxCount = minutes[m]
-print int(maxGuard),maxMinute#,minutes
-#87789 too low
-#92291 too low
+
+print int(maxGuard)*maxMinute
